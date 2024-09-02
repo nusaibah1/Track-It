d3dbaf7ea56d4a3a672519284dc1c5b4a7d54ed8
nusaibah1 <charifnusaibah01@gmail.com>
2024-09-02 14:14:57 +0200

    CORS

--- index.js
+++ index.js
@@ -17,9 +17,23 @@ app.use(router,
     express.urlencoded ({
     extended: true
     })
+
 )
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
+    next()
+})
 app.use('/user', userRouter)
+app.use('/order', userRouter)
 app.use('/item', itemRouter)
+
+
 
 
 // Endpoint
