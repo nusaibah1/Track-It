31142e4932fe6af3d3bf8460f830ec8b653f9071
nusaibah1 <charifnusaibah01@gmail.com>
2024-09-03 10:14:09 +0200

    Middleware reformatting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import path from 'path'
-import {connection as db} from './config/config.js' 
+// import {connection as db} from './config/config.js' 
 import { userRouter} from './controller/userController.js'
 import  {itemRouter} from './controller/itemController.js'
 
@@ -11,15 +11,6 @@ const port = +process.env.PORT || 4000
 const router = express.Router()
 
 //Middleware 
-app.use(router, 
-    express.static('./static'),
-    express.json(),
-    express.urlencoded ({
-    extended: true
-    })
-
-)
-
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
@@ -29,13 +20,20 @@ app.use((req, res, next) => {
     res.header("Access-Control-Expose-Headers", "Authorization");
     next()
 })
+app.use(router, 
+    express.static('./static'),
+    express.json(),
+    express.urlencoded ({
+    extended: true
+    })
+
+)
+
 app.use('/user', userRouter)
 app.use('/order', userRouter)
 app.use('/item', itemRouter)
 
-
-
-
+g
 // Endpoint
 router.get('^/$|/TrackIt', (req, res) => {
     res.status(200).sendFile(path.resolve('./static/html/index.html'))
